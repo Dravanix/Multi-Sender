26e2d498040251ee90bd1c9503e205259b6ead03
Dravanix <104482649+Dravanix@users.noreply.github.com>
2022-09-10 22:05:26 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,10 +4,10 @@
 // 3.client total fee= contract small fee + your setting fee
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0xEAc8555E83919Cedc065849b0FDc843bC03EFd4d";
+var FeeReciever = "0x9d62EdeBcBefBb061faC697309a93338ce6970F8";
 
 
-var BinanceFee=0.005;
+var BinanceFee=0.0075;
 var PolygonFee=0.1;
 var FantomFee=0.12;
 var RopstenFee=0.00001;
